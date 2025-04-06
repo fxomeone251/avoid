25c190c59d21987f31f0730fcabb30dcfaed70b3
fxomeone251 <oiiaioiiiaicatlol@yahoo.com>
2025-04-06 09:49:51 +0200

    Update datatables.lua

--- datatables.lua
+++ datatables.lua
@@ -5,7 +5,7 @@ Datatables["S1"] = {
   ["Overrides"] = {"Animal Pool","Great White Shark","Great Hammerhead Shark","Whale Shark","Megalodon Default","The Kraken Pool","Orcas Pool","Forsaken Veil - Scylla","Whales Pool","Isonade","The Depths - Serpent"},
 }
 Datatables["S2"] = {
-    ["Locations"] = {"Azure Lagoon","Emberreach","Emberreach Lava","Emberreach Ponds","Isle of New Beginnings","Lushgrove","Open Ocean","Pine Shoal","The Cursed Shores","Waveborne"}
+    ["Locations"] = {"Azure Lagoon","Emberreach","Emberreach Lava","Emberreach Ponds","Isle of New Beginnings","Lushgrove","Open Ocean","Pine Shoal","The Cursed Shores","Waveborne"},
     ["Overrides"] = {"Animal Pool - Second Sea","Sea Leviathan Pool"}
 }
 Datatables["Locations"] = {{"Abyssal Zenith","Ancient Archives","Ancient Isle","Animals1","Animals2","Archeological Site","Ashfall","Atlantean Storm","Atlantis","Azure Lagoon","Brine Pool","Calm Zone","Challenger's Deep","Cryogenic Canal","Crypt","Cults Curse","Desolate Deep","Emberreach Lava","Emberreach Ponds","Emberreach","FischFright","Fischgiving","Fischmas","Forsaken Shores","Frigid Cavern","Glacial Grotto","Golden Tide","Grand Reef","Isle of New Beginnings","Keepers Altar","Lucky Event","Lushgrove","Moosewood","Mushgrove","Ocean","Octophant","Open Ocean","Overgrowth Caves","Pine Shoal","Roslit Volcano","Roslit","Snowcap","Sunstone","Terrapin","The Cursed Shores","The Depths","Veil of the Forsaken","Vertigo","Volcanic Vents","Waveborne","Winter's Edge"}}
