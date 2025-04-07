c199f5368c2b5889e404b1e8b18f936a10f817f3
fxomeone251 <oiiaioiiiaicatlol@yahoo.com>
2025-04-07 18:41:39 +0200

    Update datatables.lua

--- datatables.lua
+++ datatables.lua
@@ -6,7 +6,7 @@ Datatables["S1"] = {
 }
 Datatables["S2"] = {
     ["Locations"] = {"Azure Lagoon","Emberreach","Emberreach Lava","Emberreach Ponds","Isle of New Beginnings","Lushgrove","Open Ocean","Pine Shoal","The Cursed Shores","Waveborne"},
-    ["Overrides"] = {"Animal Pool - Second Sea","Sea Leviathan Pool"}
+    ["Overrides"] = {"Animal Pool","Sea Leviathan Pool"} --hopefully
 }
 Datatables["Locations"] = {"Abyssal Zenith","Ancient Archives","Ancient Isle","Animals1","Animals2","Archeological Site","Ashfall","Atlantean Storm","Atlantis","Azure Lagoon","Brine Pool","Calm Zone","Challenger's Deep","Cryogenic Canal","Crypt","Cults Curse","Desolate Deep","Emberreach Lava","Emberreach Ponds","Emberreach","FischFright","Fischgiving","Fischmas","Forsaken Shores","Frigid Cavern","Glacial Grotto","Golden Tide","Grand Reef","Isle of New Beginnings","Keepers Altar","Lucky Event","Lushgrove","Moosewood","Mushgrove","Ocean","Octophant","Open Ocean","Overgrowth Caves","Pine Shoal","Roslit Volcano","Roslit","Snowcap","Sunstone","Terrapin","The Cursed Shores","The Depths","Veil of the Forsaken","Vertigo","Volcanic Vents","Waveborne","Winter's Edge"}
 
@@ -14,5 +14,8 @@ table.insert(Datatables["S1"]["Overrides"], "Octophant Pool Without Elephant")
 table.insert(Datatables["S1"]["Overrides"], "Octophant Pool With Elephant")
 table.insert(Datatables["S2"]["Overrides"], "Octophant Pool Without Elephant")
 table.insert(Datatables["S2"]["Overrides"], "Octophant Pool With Elephant")
+
+table.insert(Datatables["S1"]["Overrides"], "Octophant Pool") -- hopefully
+table.insert(Datatables["S2"]["Overrides"], "Octophant Pool") -- hopefully
 
 return Datatables
