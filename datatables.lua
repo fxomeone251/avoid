823569508ff7eb6a27552498670d888f7eab8d2d
fxomeone251 <oiiaioiiiaicatlol@yahoo.com>
2025-04-12 14:02:02 +0200

    Update datatables.lua

--- datatables.lua
+++ datatables.lua
@@ -10,9 +10,4 @@ Datatables["S2"] = {
 }
 Datatables["Locations"] = {"Abyssal Zenith","Ancient Archives","Ancient Isle","Animals1","Animals2","Archeological Site","Ashfall","Atlantean Storm","Atlantis","Azure Lagoon","Blue Moon - First Sea","Blue Moon - Second Sea","Brine Pool","Calm Zone","Challenger's Deep","Cryogenic Canal","Crypt","Cults Curse","Desolate Deep","Emberreach Lava","Emberreach Ponds","Emberreach","FischFright","Fischgiving","Fischmas","Forsaken Shores","Frigid Cavern","Glacial Grotto","Golden Tide","Grand Reef","Isle of New Beginnings","Keepers Altar","Lucky Event","Lushgrove","Moosewood","Mushgrove","Ocean","Octophant","Open Ocean","Overgrowth Caves","Pine Shoal","Roslit Volcano","Roslit","Snowcap","Sunstone","Terrapin","The Cursed Shores","The Depths","Veil of the Forsaken","Vertigo","Volcanic Vents","Waveborne","Winter's Edge"}
 
-table.insert(Datatables["S1"]["Overrides"], "Octophant Pool Without Elephant")
-table.insert(Datatables["S1"]["Overrides"], "Octophant Pool With Elephant")
-table.insert(Datatables["S2"]["Overrides"], "Octophant Pool Without Elephant")
-table.insert(Datatables["S2"]["Overrides"], "Octophant Pool With Elephant")
-
 return Datatables
