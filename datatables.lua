168e49133fafb2f8b9488be48e56962e13a10f56
fxomeone251 <oiiaioiiiaicatlol@yahoo.com>
2025-04-14 19:21:58 +0200

    uhh

--- datatables.lua
+++ datatables.lua
@@ -13,10 +13,9 @@ Datatables["S2"] = {
 Datatables["Locations"] = {"Abyssal Zenith","Ancient Archives","Ancient Isle","Animals1","Animals2","Archeological Site","Ashfall","Atlantean Storm","Atlantis","Azure Lagoon","Blue Moon - First Sea","Blue Moon - Second Sea","Brine Pool","Calm Zone","Challenger's Deep","Cryogenic Canal","Crypt","Cults Curse","Desolate Deep","Emberreach Lava","Emberreach Ponds","Emberreach","FischFright","Fischgiving","Fischmas","Forsaken Shores","Frigid Cavern","Glacial Grotto","Golden Tide","Grand Reef","Isle of New Beginnings","Keepers Altar","Lucky Event","Lushgrove","Moosewood","Mushgrove","Ocean","Octophant","Open Ocean","Overgrowth Caves","Pine Shoal","Roslit Volcano","Roslit","Snowcap","Sunstone","Terrapin","The Cursed Shores","The Depths","Veil of the Forsaken","Vertigo","Volcanic Vents","Waveborne","Winter's Edge"}
 Datatables["Fish"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/Lythicals/script/refs/heads/main/newdatatables"))().Fish
 -- thanks lyth
-
-for i,v in pairs({"Octophant Pool Without Elephant", "Octophant Pool With Elephant"}) do
-  table.insert(Datatables["S1"], v)
-  table.insert(Datatables["S2"], v)
-end
+table.insert(Datatables["S1"], "Octophant Pool Without Elephant")
+table.insert(Datatables["S2"], "Octophant Pool With Elephant")
+table.insert(Datatables["S1"], "Octophant Pool Without Elephant")
+table.insert(Datatables["S2"], "Octophant Pool With Elephant")
 
 return Datatables
